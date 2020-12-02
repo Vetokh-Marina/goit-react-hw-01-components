938852c521dc20900f83668a0c68c4b613c23007
Vetokh-Marina <marina.vetoh@gmail.com>
2020-12-02 21:36:35 +0200

    добавляет задание 1-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import user from './components/Profile/user.json';
+import Profile from './components/Profile/profile';
+import Section from './components/Section/section';
+import Statistics from './components/Statistics/Statistics';
+import statisticalData from './components/Statistics/statistical-data.json';
 
-function App() {
+
+
+  function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Section>
+       <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    <Section>
+        <Statistics title="Upload stats" stats={statisticalData} />;
+      </Section>
+      {/* <Section>
+        <FriendList friends={friends} />;
+      </Section> */}
+      
 
+      {/* <FriendList friends={friends} /> */}
+
+      {/* <TransactionHistory items={transactions} /> */}
+    </Section>
+  );
+ }
 export default App;
---
+++ src/components/Profile/profile.js
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import s from '../Profile/profile.module.css';
+
+  
+ function Profile({ name, tag, location, avatar, stats:{followers,views,likes} }) {
+    return (
+        <div className={s.profile}>
+        <div className={s.description}>
+        <img src={avatar} alt={name} className={s.avatar} />
+        <p className={s.name}>{name}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
+
+   <ul className={s.stats}>
+        <li className={s.item}>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
+        </li>
+        <li className={s.item}>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
+        </li>
+        <li className={s.item}>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
+export default Profile;
